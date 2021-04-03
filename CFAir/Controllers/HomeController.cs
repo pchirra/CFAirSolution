96a9ffb706c607e3f9aff8d204984b64a6dbc7d2
Srikanth1807 <74841760+Srikanth1807@users.noreply.github.com>
2021-04-03 18:45:35 +0530

    first commit

--- CFAir/Controllers/HomeController.cs
+++ CFAir/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace CFAir.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Your contact page.....";
 
             return View();
         }
